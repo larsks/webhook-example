7877054a8dc997560f548def624f18d0d20a7307
Lars Kellogg-Stedman <lars@redhat.com>
2023-12-12 22:49:44 -0500

    Move signature checking code into its own function

--- app.py
+++ app.py
@@ -3,9 +3,38 @@ import hmac
 
 
 class CONFIG_DEFAULTS:
-    COLOR_SUCCESS = "#05eb2f"
-    COLOR_FAILURE = "#f00216"
+    pass
 
+class WebhookSignatureError(Exception):
+    pass
+
+def verify_webhook_signature():
+    if 'GITHUB_WEBHOOK_SECRET' not in current_app.config:
+        raise WebhookSignatureError("secret has not been configured")
+
+    if "X-Hub-Signature-256" not in request.headers:
+        raise WebhookSignatureError("secret is missing signature")
+
+    sigheader = request.headers["X-Hub-Signature-256"]
+    try:
+        sigtype, request_signature = sigheader.split("=", 1)
+    except ValueError:
+       raise WebhookSignatureError(f"invalid signature ({sigheader})")
+
+    if sigtype != "sha256":
+        raise WebhookSignatureError(f"unknown signature type ({sigtype})")
+
+    sigbuilder = hmac.HMAC(
+        key=current_app.config["GITHUB_WEBHOOK_SECRET"].encode(),
+        msg=request.data,
+        digestmod="sha256",
+    )
+    calculated = sigbuilder.hexdigest()
+
+    if calculated != request_signature:
+        raise WebhookSignatureError(
+            f"bad signature (request {request_signature}, computed {calculated})"
+        )
 
 def create_app(config_from_env=True):
     app = Flask(__name__)
@@ -15,35 +44,11 @@ def create_app(config_from_env=True):
 
     @app.route("/hook/push", methods=["POST"])
     def handle_push_notification():
-        if 'GITHUB_WEBHOOK_SECRET' not in current_app.config:
-            current_app.logger.error("secret has not been configured")
-            abort(400, "Secret has not been configured")
-        if "X-Hub-Signature-256" not in request.headers:
-            current_app.logger.error("request is missing signature")
-            abort(400, "Request is missing signature")
-        sigheader = request.headers["X-Hub-Signature-256"]
         try:
-            sigtype, sigdata = sigheader.split("=", 1)
-        except ValueError:
-            current_app.logger.error("invalid signature (%s)", sigheader)
-            abort(400, "Invalid signature")
-
-        if sigtype != "sha256":
-            current_app.logger.error("unknown signature type (%s)", sigtype)
-            abort(400, "Unknown signature type")
-
-        sigbuilder = hmac.HMAC(
-            key=current_app.config["GITHUB_WEBHOOK_SECRET"].encode(),
-            msg=request.data,
-            digestmod="sha256",
-        )
-        if sigbuilder.hexdigest() != sigdata:
-            current_app.logger.error(
-                "bad signature (request %s, computed %s)",
-                sigdata,
-                sigbuilder.hexdigest(),
-            )
-            abort(400, "Bad signature")
+            verify_webhook_signature()
+        except WebhookSignatureError as err:
+            current_app.logger.error("signature verification failed: %s", err)
+            abort(400, f"Signature verification failed: {err}")
 
         return {"status": "success"}
 
